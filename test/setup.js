1caa72d00cc547d84bdbb9b6ab3aab41f26795cd
Niall Donnelly <niall.donnelly@feedhenry.com>
2013-11-06 12:55:00 +0000

    Update of models to include field options.

--- test/setup.js
+++ test/setup.js
@@ -12,32 +12,179 @@ var models = require('../lib/common/models.js')();
 
 //Need test data for fields, pages, forms, themes, appforms, appthemes,
 
-var fieldSchemaText = {
+var fieldText = {
   "_id": "123456789",
-  "label": "SingleText",
+  "label": "SingleText Field",
   "helpText": "This is test single text Field 1.",
   "type": "singleText",
   "options": [{"key": "maxLength", "val": "100"}],
   "required": false
 };
 
-var fieldSchemaMultiText = {
+var fieldMultiText = {
   "_id": "123456788",
-  "label": "MultiText",
+  "label": "MultiText Field",
   "helpText": "This is multi text Field 1.",
   "type": "multiText",
   "options": [{"key": "maxLength", "val": "900"}],
   "required": false
 };
 
-var fieldSchemaMultiText = {
+var fieldNumber = {
   "_id": "123456787",
-  "label": "testField1",
-  "helpText": "This is test Field 1.",
-  "type": "multiText",
-  "options": [{"key": "maxLength", "val": "900"}],
+  "label": "Number Field",
+  "helpText": "This is number Field 1.",
+  "type": "number",
+  "options": [],
   "required": false
 };
+
+var fieldEmail = {
+  "_id": "123456786",
+  "label": "Email Address Field",
+  "helpText": "This is email Field 1.",
+  "type": "email",
+  "options": [],
+  "required": false
+};
+
+var fieldDropdownList = {
+  "_id": "123456785",
+  "label": "Dropdown List Field",
+  "helpText": "This is dropDownList Field 1.",
+  "type": "dropDownList",
+  "options": [{"key": "Drop Option 1", val: ""}, {"key": "Drop Option 2", "val": "selected"}], // Only one can be selected at a time. Part of validation. Has to have at least one entry.
+  "required": false
+};
+
+var fieldRadioButton = {
+  "_id": "123456784",
+  "label": "Radio Button Field",
+  "helpText": "This is Radio Button Field 1.",
+  "type": "radioButton",
+  "options": [{"key": "Radio Option 1", val: ""}, {"key": "Radio Option 2", "val": "selected"}], // Only one can be selected at a time. Part of validation. Has to have at least one entry.
+  "required": false
+};
+
+var fieldCheckbox = {
+  "_id": "123456783",
+  "label": "Checkbox Field",
+  "helpText": "This is Checkbox Field 1.",
+  "type": "checkbox",
+  "options": [{"key": "Check Option 1", val: "selected"}, {"key": "Check Option 2", "val": "selected"}], // Only one can be selected at a time. Part of validation. Has to have at least one entry.
+  "required": false
+};
+
+var fieldCheckbox = {
+  "_id": "123456782",
+  "label": "Checkbox Field",
+  "helpText": "This is Checkbox Field 1.",
+  "type": "checkbox",
+  "options": [{"key": "Check Option 1", val: "selected"}, {"key": "Check Option 2", "val": "selected"}], // Only one can be selected at a time. Part of validation. Has to have at least one entry.
+  "required": false
+};
+
+var fieldLocationLatLong = {
+  "_id": "123456781",
+  "label": "LocationLatLong Field",
+  "helpText": "This is LocationLatLong Field 1.",
+  "type": "locationLatLong",
+  "options": [], // Only one can be selected at a time. Part of validation. Has to have at least one entry.
+  "required": false
+};
+
+var fieldLocationNorthEast = {
+  "_id": "123456780",
+  "label": "fieldLocationNorthEast Field",
+  "helpText": "This is fieldLocationNorthEast Field 1.",
+  "type": "locationNorthEast",
+  "options": [], // Only one can be selected at a time. Part of validation. Has to have at least one entry.
+  "required": false
+};
+
+var fieldLocationPhoto = {
+  "_id": "123456779",
+  "label": "Photo Field",
+  "helpText": "This is Photo Field 1.",
+  "type": "photo",
+  "options": [{"key": "convertWidth", "val": "200"}, {"key" : "convertHeight", "val": "200"}], // Only one can be selected at a time. Part of validation. Has to have at least one entry.
+  "required": false
+};
+
+var fieldLocationSignature = {
+  "_id": "123456778",
+  "label": "Signature Field",
+  "helpText": "This is Signature Field 1.",
+  "type": "signature",
+  "options": [{"key": "convertWidth", "val": "200"}, {"key" : "convertHeight", "val": "200"}], // Only one can be selected at a time. Part of validation. Has to have at least one entry.
+  "required": false
+};
+
+var fieldLocationFile = {
+  "_id": "123456777",
+  "label": "File Field",
+  "helpText": "This is File Field 1.",
+  "type": "file",
+  "options": [], // Only one can be selected at a time. Part of validation. Has to have at least one entry.
+  "required": false
+};
+
+var fieldDate = {
+  "_id": "123456776",
+  "label": "Date Field",
+  "helpText": "This is Date Field 1.",
+  "type": "date",
+  "options": [], // Only one can be selected at a time. Part of validation. Has to have at least one entry.
+  "required": false
+};
+
+var fieldTime = {
+  "_id": "123456775",
+  "label": "Time Field",
+  "helpText": "This is Time Field 1.",
+  "type": "time",
+  "options": [], // Only one can be selected at a time. Part of validation. Has to have at least one entry.
+  "required": false
+};
+
+var fieldDateTime = {
+  "_id": "123456774",
+  "label": "DateTime Field",
+  "helpText": "This is DateTime Field 1.",
+  "type": "dateTime",
+  "options": [], // Only one can be selected at a time. Part of validation. Has to have at least one entry.
+  "required": false
+};
+
+var fieldSectionBreak = {
+  "_id": "123456773",
+  "label": "SectionBreak Field",
+  "helpText": "This is SectionBreak Field 1.",
+  "type": "sectionBreak",
+  "options": [{"key": "sectionTitle", "val": "Some Section Title"}, {"key": "sectionDescription", "val": "I am a section."}], // Only one can be selected at a time. Part of validation. Has to have at least one entry.
+  "required": false
+};
+
+
+///////////////////////// PAGES//////////////////////
+
+var pageTest1 = {
+  "_id" : "page1ID123456789",
+  "name" : "The First Page",
+  "description" : "The First Page Description.",
+  "fields": ["123456789", "123456788", "123456787"]
+}
+
+var pageTest2 = {
+  "_id" : "page2ID123456788",
+  "name" : "The Second Page",
+  "description" : "The Second Page Description.",
+  "fields": ["123456786", "123456785", "123456784"]
+}
+
+
+//////////////////////FORMS ///////////////////////////
+
 
 exports.globalSetUp = function(test, assert){
 
@@ -54,10 +201,11 @@ exports.globalSetUp = function(test, assert){
 
 exports.globalTearDown = function(test, assert){
   console.log("tearDown Called");
-  cleanDatabase(assert, function(err){
-    if(err) console.log(err);
-    test.finish();
-  });
+//  cleanDatabase(assert, function(err){
+//    if(err) console.log(err);
+//    test.finish();
+//  });
+  test.finish();
 }
 
 function cleanDatabase(assert, cb){
@@ -105,7 +253,11 @@ function setUpDatabase(assert, cb){
 
 function addTestData(db, cb){
 
- return cb();
+  //Get mongoose connection,
+  //Set up models
+  //var connection = mongoose._getConnection()
+
+  return cb();
 }
 
 
--- test/test_submitFormData.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created with JetBrains WebStorm.
- * User: ndonnelly
- * Date: 05/11/2013
- * Time: 09:02
- * To change this template use File | Settings | File Templates.
- */
