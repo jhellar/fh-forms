27a5fef7f6da0c25ecdb8b06caa7c1a0dc695031
Martin Murphy <martin.murphy@feedhenry.com>
2013-11-07 12:30:27 +0000

    updated tests

--- test/test_setBasicForm.js
+++ test/test_setBasicForm.js
@@ -8,9 +8,41 @@ var initDatabase = require('./setup.js').initDatabase;
 
 var options = {'uri': process.env.FH_DOMAIN_DB_CONN_URL, userEmail: "testUser@example.com"};
 
-var emptyForm = require('./Fixtures/emptyform.json');
-
 var connection;
+var formModel;
+
+var TEST_FORM_SIMPLE = {
+    "name": "Simple Empty Form",
+    "description": "This is my form. It has no fields!",
+    "lastUpdated": "2013-10-16 06:13:52",
+    "pages": [],
+    "fieldRules": [],
+    "pageRules": []
+};
+var TEST_FORM_SIMPLE_TO_BE_UPDATED = {
+    "name": "Simple Empty Form",
+    "description": "This is my form. It has no fields!",
+    "lastUpdated": "2013-10-16 06:13:52",
+    "pages": [],
+    "fieldRules": [],
+    "pageRules": []
+};
+var TEST_FORM_NO_NAME = {
+    "description": "This is my form. It has no fields!",
+    "lastUpdated": "2013-10-16 06:13:52",
+    "pages": [],
+    "fieldRules": [],
+    "pageRules": []
+};
+var TEST_FORM_NO_DESCR = {
+    "name": "Simple Empty Form",
+    "lastUpdated": "2013-10-16 06:13:52",
+    "pages": [],
+    "fieldRules": [],
+    "pageRules": []
+};
+var TEST_FORM_EMPTY_OBJ = {};
+
 
 module.exports.initialize = function(test, assert){
   initDatabase(assert, function(err) {
@@ -28,48 +60,147 @@ module.exports.finalize = function(test, assert){
 };
 
 module.exports.setUp = function(test, assert){
+  formModel = models.get(connection, models.MODELNAMES.FORM);
   test.finish();
 };
 
 module.exports.tearDown = function(test, assert){
-  console.log('tearDown');
   forms.tearDownConnection(options, function (err) {
     assert.ok(!err);
     test.finish();
   });
 };
 
-module.exports.testAddForm = function(test, assert){
-  //console.log(forms);
+function assertFormNamedNotFound(assert, name, msg, cb) {
+  formModel.findOne({name: name}, function (err, data) {
+    assert.ok(!err, 'should not return error: ' + util.inspect(err));
+    assert.isNull(data, msg + util.inspect(data));
+    cb();
+  });
+}
 
-  var formModel = models.get(connection, models.MODELNAMES.FORM);
+function assertFormNamedFound(assert, name, msg, cb) {
+  formModel.findOne({name: name}, function (err, data) {
+    assert.ok(!err, 'should not return error: ' + util.inspect(err));
+    assert.isNotNull(data, msg + util.inspect(data));
+    cb();
+  });
+}
+
+module.exports.testUpdateForm = function(test, assert){
+  var myOrigForm = JSON.parse(JSON.stringify(TEST_FORM_SIMPLE_TO_BE_UPDATED));
+  var myUpdForm = JSON.parse(JSON.stringify(TEST_FORM_SIMPLE_TO_BE_UPDATED));
+  myOrigForm.name += "Original " + Date.now();
+  myUpdForm.name += "Updated " + Date.now();
 
   async.series([
+    async.apply(assertFormNamedNotFound, assert, myOrigForm.name, 'should not have found form - not added yet - found: '),
+    async.apply(assertFormNamedNotFound, assert, myUpdForm.name, 'should not have found form - not updated yet - found: '),
     function(cb) {
-      console.log('testUpdateForm() - 1 ');
-      formModel.findOne({name: emptyForm.name}, function (err, data) {
+      forms.updateForm(options, myOrigForm, function(err, doc){
+        assert.ok(!err, 'testUpdateForm() - error fom updateForm: ' + util.inspect(err));
+        assert.ok(doc._id, 'document should have been returned with an id - doc: ' + util.inspect(doc._id));
+        myUpdForm._id = doc._id;
+        cb();
+      });
+    },
+    async.apply(assertFormNamedFound, assert, myOrigForm.name, 'should have found form - data: '),
+    async.apply(assertFormNamedNotFound, assert, myUpdForm.name, 'should not have found form - not updated yet - found: '),
+    function(cb) {
+      forms.updateForm(options, myUpdForm, function(err, doc){
+        assert.ok(!err, 'testUpdateForm() - error fom updateForm: ' + util.inspect(err));
+        assert.ok(doc._id, 'document should have been returned with an id - doc: ' + util.inspect(doc._id));
+        cb();
+      });
+    },
+    async.apply(assertFormNamedNotFound, assert, myOrigForm.name, 'should not have found form - has been renamed - found: '),
+    async.apply(assertFormNamedFound, assert, myUpdForm.name, 'should have found updated form - data: ')  
+  ], function(err){
+    assert.ok(!err);
+    test.finish();
+  });
+};
+
+module.exports.testAddForm = function(test, assert) {
+  async.series([
+    function(cb) {
+      formModel.findOne({name: TEST_FORM_SIMPLE.name}, function (err, data) {
         assert.ok(!err, 'should not returned error: ' + util.inspect(err));
         assert.isNull(data, 'should not have found form - haven\'t added it yet - found: ' + util.inspect(data));
         cb();
       });
     },
     function(cb) {
-      console.log('testUpdateForm() - 2 ');
-      forms.updateForm(options, emptyForm, function(err, doc){
-        console.log("testUpdateForm() - in callback");
+      forms.updateForm(options, TEST_FORM_SIMPLE, function(err, doc){
         assert.ok(!err, 'testUpdateForm() - error fom updateForm: ' + util.inspect(err));
         cb();
       });
     },
     function(cb) {
-      console.log('testUpdateForm() - 3 ');
-      formModel.findOne({name: emptyForm.name}, function (err, data) {
+      formModel.findOne({name: TEST_FORM_SIMPLE.name}, function (err, data) {
         assert.ok(!err, 'should have found form');
-        assert.strictEqual(data.formDescription, emptyForm.formDescription, "new description should ahve been added");
+        assert.strictEqual(data.formDescription, TEST_FORM_SIMPLE.formDescription, "new description should ahve been added");
         assert.strictEqual(data.updatedBy, options.userEmail, "updatedBy field should have been set to userEmail");
         cb();
       });
-    },
+    }
+  ], function(err){
+    assert.ok(!err);
+    test.finish();
+  });
+};
+
+module.exports.testInValidForm1 = function(test, assert){
+  async.series([
+    function(cb) {
+      forms.updateForm(options, TEST_FORM_EMPTY_OBJ, function(err, doc){
+        assert.ok(err, 'testUpdateForm() - updateForm should have returned error: ' + util.inspect(err));
+        cb();
+      });
+    }
+  ], function(err){
+    assert.ok(!err);
+    test.finish();
+  });
+};
+
+module.exports.testInValidForm2 = function(test, assert){
+  async.series([
+    function(cb) {
+      forms.updateForm(options, null, function(err, doc){
+        assert.ok(err, 'testUpdateForm() - updateForm should have returned error: ' + util.inspect(err));
+        cb();
+      });
+    }
+  ], function(err){
+    assert.ok(!err);
+    test.finish();
+  });
+};
+
+module.exports.testInValidForm3 = function(test, assert){
+  async.series([
+    function(cb) {
+      forms.updateForm(options, TEST_FORM_NO_DESCR, function(err, doc){
+        assert.ok(err, 'testUpdateForm() - updateForm should have returned error: ' + util.inspect(err));
+        cb();
+      });
+    }
+  ], function(err){
+    assert.ok(!err);
+    test.finish();
+  });
+};
+
+
+module.exports.testInValidForm4 = function(test, assert){
+  async.series([
+    function(cb) {
+      forms.updateForm(options, TEST_FORM_NO_NAME, function(err, doc){
+        assert.ok(err, 'testUpdateForm() - updateForm should have returned error: ' + util.inspect(err));
+        cb();
+      });
+    }
   ], function(err){
     assert.ok(!err);
     test.finish();
