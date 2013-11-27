59a9e98e2f11ae3f81c18b628c6f5c73b68cd352
Martin Murphy <martin.murphy@feedhenry.com>
2013-11-27 12:51:03 +0000

    add initial empty failing test

---
+++ test/accept/test_forms_rules_engine.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+var async = require('async');
+var util = require('util');
+
+var assert = require('assert');
+
+module.exports.testBasicFormRules = function (finish) {
+
+  assert.fail("must implement test");
+  
+};
+
