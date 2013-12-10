2bedc2f1305a7bba2bb087423586518cca6335d7
Martin Murphy <martin.murphy@feedhenry.com>
2013-12-10 12:47:14 +0000

    update closure to avoid async module using "define"

--- client/prefix.js
+++ client/prefix.js
@@ -1,6 +1,6 @@
 /* This is the prefix file */
 function rulesEngine (formDef) {
-  
+  var define = null;
   var module = {exports:{}}; // create a module.exports - async will load into it
 
 /* End of prefix file */
