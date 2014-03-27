4c2ce81d921d1676bf3a2357d5b56469be1bb390
craig <craig.brookes@feedhenry.com>
2014-03-27 17:28:46 +0000

    update validation for validate immediately

--- client/prefix.js
+++ client/prefix.js
@@ -1,4 +1,8 @@
 /* This is the prefix file */
+if(appForm){
+  appForm.RulesEngine=rulesEngine;
+}
+
 function rulesEngine (formDef) {
   var define = {};
   var module = {exports:{}}; // create a module.exports - async will load into it
