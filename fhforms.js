e64563a30cf532551a6feca47b73e88a682892ad
Niall Donnelly <niall.donnelly@feedhenry.com>
2016-07-05 11:57:02 +0100

    Moved fh-forms mongoose intialisation to the top level. Also refactored to an initialisation function to pass the logger

--- fhforms.js
+++ fhforms.js
@@ -1,7 +1,24 @@
+var mongoose = require('mongoose');
+var clsMongoose = require('cls-mongoose');
+var log = require('./lib/common/logger');
 var forms = require('./lib/forms.js');
 var formsMiddleware = require('./lib/middleware.js');
 
-
 module.exports.core = forms;
 module.exports.middleware = formsMiddleware;
+
+module.exports.setLogger = function setFormsLogger(logger) {
+
+  //fh-forms needs a logger.
+  if (!logger) {
+    return;
+  }
+
+  log.setLogger(logger);
+
+  if (logger.getLoggerNamespace) {
+    var ns = logger.getLoggerNamespace();
+    clsMongoose(ns, mongoose);
+  }
+};
 module.exports.CONSTANTS = require('./lib/common/constants');
